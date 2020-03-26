3e460cec54a7ac6c34495d20ca4d1445cf2dcc4d
alexvanhell <alejandrodnl.cv@hotmail.com>
2020-03-25 19:16:28 -0600

    Added perimeter and diagonal functions for square

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const getTriangleArea = require('./functions/triangle-area');
 const getCircleArea = require('./functions/circle-area');
+const squareOperations = require('./functions/square-operations');
 
 console.log('Test');
 
 console.log('El area de mi triangulo es: ', getTriangleArea(5, 4));
 console.log('El area de mi circulo es: ', getCircleArea(5));
+
+console.log('El area del cuadrado es:', squareOperations.getArea(5.3));
+
+console.log('El perimetro del cuadrado es:', squareOperations.getPerimeter(5.3));
+console.log('La diagonal del cuadrado es:', squareOperations.getDiagonal(5.3));
